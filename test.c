babf61c2a588b38331a01b6ded19f646c494b543
shiqiang.chu <shiqiang.chu@gmail.com>
2020-06-07 00:33:09 -0600

    Feat: added foo(int)

--- test.c
+++ test.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+void foo(int count)
+{
+   printf("%d arguments passed in", count);
+}
+
 int main(int argc, char *argv[])
 {
     printf("Hello World, %s\n", argv[0]);
+
+    foo(argc);
 
     return 0;
 }
